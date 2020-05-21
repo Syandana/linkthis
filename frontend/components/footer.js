2ab68f980333fabcf3cf651bec2acdbcf2c2a291
Syandana <rizqy.syandana.p@gmail.com>
2020-05-21 20:21:08 +0700

    nightly build

---
+++ frontend/components/footer.js
@@ -0,0 +1,49 @@
+const Footer = () => (
+  <div className="container">
+    <hr />
+    <div className="row">
+      <div className="col-lg">
+        <p>Company</p>
+        <ul className="list-unstyled">
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+        </ul>
+      </div>
+      <div className="col-lg">
+        <p>Company</p>
+        <ul className="list-unstyled">
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+        </ul>
+      </div>
+      <div className="col-lg">
+        <p>Company</p>
+        <ul className="list-unstyled">
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+        </ul>
+      </div>
+      <div className="col-lg">
+        <p>Company</p>
+        <ul className="list-unstyled">
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+          <li className="text-muted">Asd</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+)
+
+export default Footer
